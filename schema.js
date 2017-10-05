65ba3ada9134a735ded02b1b969a656476b03f9a
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-05 08:26:24 +0530

    Added a file which will contain DB schema later

---
+++ schema.js
@@ -0,0 +1,22 @@
+// Agenda of this file is to note down dteps performe on mysql DB. This will later be converted into schema file.
+
+// 1. create database messaging;
+
+// 2. use messaging;
+
+/* 3. CREATE TABLE IF NOT EXISTS user
+        (
+            id INT NOT NULL UNIQUE,
+            name VARCHAR(100) NOT NULL,
+            mobile INT NOT NULL UNIQUE,
+            password VARCHAR(100),
+            is_active TINYINT(1),
+            is_enabled TINYINT(1),
+            PRIMARY KEY(id)
+        );
+*/
+// 4.  ALTER TABLE user CHANGE COLUMN name first_name VARCHAR(100) NOT NULL;
+
+// 5.  ALTER TABLE user ADD COLUMN last_name VARCHAR(100) NOT NULL AFTER first_name;
+
+// 6.  ALTER TABLE user CHANGE COLUMN id id INT UNIQUE NOT NULL AUTO_INCREMENT;
