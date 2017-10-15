c99cb450c875f0c349e3d655dc16b00a636b6dce
Kanika Sharma <kanikash4@gmail.com>
2017-10-16 02:18:28 +0530

    Cleaning code

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,13 @@
 
 var error = require('./error');
 var respond = require('./respond');
-var db = require('../db_model');
+var user = require('./user');
 
 module.exports = function(app) {
 	app.all('/_status', function(req, res, next) {
 	  res.sendStatus(200);
 	});
 
-	app.post('/signup', function (req, res, next) {
-		var dbQuery = "INSERT INTO user (first_name, last_name, email, mobile, password, is_active, is_enabled) VALUES ('" + req.body.firstName + "', '" + req.body.lastName + "', '" + req.body.email + "', " + req.body.mobile + ", '" + req.body.password + "', 1, 1);"
-	    db.query(dbQuery, function(e,r) {
-		if(e){ return next(e)}
-		res.sendStatus(200);
-		next();
-	}, 
-	// respond, 
-	error);
-	});
+	app.post('/signup', user.userSignUp, respond, error);
 
 };
