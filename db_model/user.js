97871eddc588ebe36d93bf1fbfbde9b18138ac74
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-10-25 08:19:22 +0530

    Fixed issue with first and last name not getting saved properly

--- db_model/user.js
+++ db_model/user.js
@@ -5,7 +5,7 @@ var db = require('./db');
 var user = {
     createUser : function(userDetails, cb){
         console.log("user details received: ", userDetails);
-		var dbQuery = "INSERT INTO user (first_name, last_name, email, mobile, password, is_active, is_enabled) VALUES ('" + userDetails.firstName + "', '" + userDetails.lastName + "', '" + userDetails.email + "', " + userDetails.mobile + ", '" + userDetails.password + "', 1, 1);";
+		var dbQuery = "INSERT INTO user (first_name, last_name, email, mobile, password, is_active, is_enabled) VALUES ('" + userDetails.first_name + "', '" + userDetails.last_name + "', '" + userDetails.email + "', " + userDetails.mobile + ", '" + userDetails.password + "', 1, 1);";
         db.query(dbQuery, function(e,r){
             if(e){return cb(e);}
             cb(null, r);
